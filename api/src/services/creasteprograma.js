525f0c16ed71eec70142125a0d06ea64ab971018
Carlos Ramos González <emugameplay.tv@gmail.com>
2022-12-02 23:23:42 +0000

    kk

--- api/src/services/creasteprograma.js
+++ api/src/services/creasteprograma.js
@@ -3,7 +3,7 @@ const { Programas, Facultades } = require('../db');
 function createprograma(params) {
 
      params.map(async (e) => {
-          const relacion = await Facultades.findOne({ where: { NombreFacultad: e.NombreFacultad } })
+          const relacion = await Facultades.findOne({ where: { NombreFacultad: e.NombreFacultad } }).cath((err) => console.log(err))
 
          const pro= await Programas.create({
                NombrePrograma: e.NombrePrograma,
