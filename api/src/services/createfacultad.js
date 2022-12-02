4c649f8b0ea27a0b005d4af7cf3a83491382fe69
Carlos Ramos González <emugameplay.tv@gmail.com>
2022-12-02 22:21:06 +0000

    ññ

--- api/src/services/createfacultad.js
+++ api/src/services/createfacultad.js
@@ -1,22 +1,18 @@
 const { Facultades } = require("../db");
 
-const crearfacultad = (params) => {
+const crearfacultad= (params)=> {
+    
+ params.map( async(facultad) => {
+    
+   await Facultades.findOrCreate({
+        where: { NombreFacultad: facultad.NombreFacultad },
+   })
+ 
+ })
 
-    params.map(async (facultad) => {
+        
 
-       /*  let dato = await Facultades.findAll({
-            where: {
-                NombreFacultad: facultad.NombreFacultad
-            }
-        })
-        if (dato.lenth === 0) { } */
-        await Facultades.create({ NombreFacultad: facultad.NombreFacultad });
-
-    })
-
-
-
-    return "saved facultades";
+   return "saved facultades";     
 }
 
 module.exports = crearfacultad;
