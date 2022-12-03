a52ee11908ffa6ae976f1cd3691d43267b67db28
Carlos Ramos González <emugameplay.tv@gmail.com>
2022-12-03 00:20:26 +0000

    sd

--- api/src/services/cretepemsun.js
+++ api/src/services/cretepemsun.js
@@ -6,8 +6,13 @@ const createpemsun= async(params)=> {
 for (let index = 0; index < params.length; index++) {
     const element = params[index];
     const relacion = await Programas.findOne({ where: { NombrePrograma: element.NombrePrograma } })
+    const pensum = await Pensums.create({
+        Pensum: element.Pensum,
+        Semestres: element.Semestres,
+    })
+    console.log(pensum)
+ 
     
-    console.log(relacion)
 }
 
 
