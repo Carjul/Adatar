b04f4309a7331766b09c7eb66a51ffdbafd38818
Carlos Ramos González <emugameplay.tv@gmail.com>
2022-12-11 16:56:45 +0000

    data limit

--- api/src/app.js
+++ api/src/app.js
@@ -5,20 +5,17 @@ const morgan =require("morgan");
 const { rutaUpload } = require("./routes/uploadFile");
 const schema  = require("./graphql/Schema");
 
-
 const app = express()
-const {PORT}=process.env
 
-app.set('port', PORT)
+app.set('port', process.env.PORT)
+
 app.use(cors())
 app.use(morgan('dev'))
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
+
 app.use('/',rutaUpload)
-app.use('/', graphqlHTTP({
-    schema:schema,
-    graphiql:true
-}))
+app.use('/', graphqlHTTP({ schema, graphiql:true }))
 
 
 
