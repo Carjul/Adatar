4af5173682a05c69095ff5e7fc638a910daf27f0
Carlos <Carlosrgonzalez88@gmail.com>
2023-01-05 22:54:47 -0500

    err

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route,Switch,Redirect} from "react-router-dom";
+import { Route,Switch } from "react-router-dom";
 import Upload from './components/File';
 import Home from './components/Home';
 import Init from './components/landing';
