3b1c6934ccec1ceda6cfbf009e337b85f1661aa5
Carlos <Carlosrgonzalez88@gmail.com>
2023-01-06 10:11:33 -0500

    passport

--- api/src/config/passport.js
+++ api/src/config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const { Users} = require('../db')
+const { Users } = require('../db')
 
 passport.use(new LocalStrategy(
     {
@@ -9,14 +9,18 @@ passport.use(new LocalStrategy(
         passwordField: 'password',
     },
     async (req, email, password, done) => {
-        const user = await Users.findOne({ where: { Email: email } }).catch((err) => { console.log(err) });
-        if (user) {
-            return done(null, user)
-        }
-        else {
-            const { email, picture, name } = req.body;
-            const create = await Users.create({ Avatar: picture,Nombre: name, Email: email, Password: password,RolId:2});
-            return done(null, create);
+        try {
+            const user = await Users.findOne({ where: { Email: email } })
+            if (user) {
+                user.password === password ? done(null, user) : done(null, false)
+            }
+            if (!user) {
+                const { email, picture, name } = req.body;
+                const usercreate = await Users.create({ Avatar: picture, Nombre: name, Email: email, Password: password, RolId: 2 });
+                return done(null, usercreate);
+            }
+        } catch (error) {
+            console.log(error)
         }
     }
 ));
@@ -31,7 +35,7 @@ passport.deserializeUser((id, done) => {
     Users.findByPk(id).then((user) => {
         done(null, user);
     })
-    .catch((err) => {
-        done(new Error(err));
-    })
+        .catch((err) => {
+            done(new Error(err));
+        })
 });
