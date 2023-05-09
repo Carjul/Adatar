ea4c43a2313a8b9aee68fa84851bb404b0712cd9
Carlos Ramos González <emugameplay.tv@gmail.com>
2023-05-09 16:56:09 +0000

    werwe

--- api/src/routes/uploadFile.js
+++ api/src/routes/uploadFile.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const upload = require("../config/multer");
 const { UploadFile } = require("../controllers/ControllerUpload");
-const { isAuthenticated } = require("../helper");
+
 
 const rutaUpload= Router()
 
-rutaUpload.post('/upload',isAuthenticated,upload,UploadFile)
+rutaUpload.post('/upload',upload,UploadFile)
 
 module.exports={
     rutaUpload
