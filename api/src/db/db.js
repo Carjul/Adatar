cdb415d95a458ca9f411a3265b7862cb0aea2961
Carjul <emugameplay.tv@gmail.com>
2025-02-09 13:54:15 -0500

    Update database connection configuration for production and development environments

--- api/src/db/db.js
+++ api/src/db/db.js
@@ -3,15 +3,26 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB, DB_PORT } = process.env;
-
+//production
 const db = new Pool({
   user: DB_USER,
   host: DB_HOST,
   password: DB_PASSWORD,
   database: DB,
   port: DB_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+  }
+}); 
+//dev
+/* const db = new Pool({
+  user: DB_USER,
+  host: DB_HOST,
+  password: DB_PASSWORD,
+  database: DB,
+  port: DB_PORT,
 });
-
+ */
 async function insertInitialData(client) {
   const query = `
 -- Insert roles in Rols table
